af1c94f62ad235228bdead733d4a8465036f031d
ShaToyaJones <jshatoyaj@yahoo.com>
2017-11-07 18:06:46 -0500

    adding folders and files

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("bacon special", "1");
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("double bacon special", "1");
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("the what is you doin burger", "2");
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("gimmie dat burger", "2");
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("feelin hot hot hot!", "1");
