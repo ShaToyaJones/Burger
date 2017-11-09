9b0d8649c2ebcbe4b164b274372380980be63c88
ShaToyaJones <jshatoyaj@yahoo.com>
2017-11-09 15:47:03 -0500

    trying to get node to work

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(255) NOT NULL,
-	devoured BOOLEAN NOT NULL,
+	devoured BOOLEAN DEFAULT false,
 	date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (id)
 );
